6a55ab83e47c6d70529895bcf2f2fc2799590814
user <user@192.168.1.20>
2021-06-23 09:48:26 -0500

    Complete Input01

--- emma/src/com/huaxia/emma/homeworks/Input01.java
+++ emma/src/com/huaxia/emma/homeworks/Input01.java
@@ -12,14 +12,17 @@ public class Input01 {
 		System.out.println(result);
 
 //		result = result * 2; // result is now 4
-		result *=2; // result is now 4
+		result *= 2; // result is now 4
 		System.out.println(result);
 
-		result = result / 2; // result is now 2
+		// result = result / 2; // result is now 2
+		result /= 2; // result is now 2
 		System.out.println(result);
 
-		result = result + 8; // result is now 10
-		result = result % 7; // result is now 3
+		// result = result + 8; // result is now 10
+		result += 8; // result is now 10
+		// result = result % 7; // result is now 3
+		result %= 7; // result is now 3
 		System.out.println(result);
 	}
 
