5865f95521ae8559c913602c76fb0bdc27f483af
hongkaizhang11 <hongkaizhang11@gmail.com>
2021-06-24 11:05:14 -0500

    actually testing src file and commiting it

---
+++ hongkai/src/com/huaxia/hongkai/homework/LoopHW01.java
@@ -0,0 +1,10 @@
+package com.huaxia.hongkai.homework;
+
+public class LoopHW01 {
+
+	public static void main(String[] args) {
+		// not real
+
+	}
+
+}
---
+++ hongkai/src/com/huaxia/hongkai/homework/NotHomeworkTest.java
@@ -0,0 +1,12 @@
+package com.huaxia.hongkai.homework;
+
+public class NotHomeworkTest {
+
+	public static void main(String[] args) {
+		System.out.println("TESTING TESTING 123");
+
+	}
+
+}
+
+// is the src file working
