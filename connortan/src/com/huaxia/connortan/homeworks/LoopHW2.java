1bed1a91271a850738bd68b3a5c5f8532e15aab0
connortan1 <86364274+connortan1@users.noreply.github.com>
2021-06-26 08:50:55 -0500

    Update LoopHW2.java

--- connortan/src/com/huaxia/connortan/homeworks/LoopHW2.java
+++ connortan/src/com/huaxia/connortan/homeworks/LoopHW2.java
@@ -3,11 +3,10 @@ package com.huaxia.connortan.homeworks;
 public class LoopHW2 {
 
 	public static void main(String[] args) {
-		for (int i = 1/2; i <= 10; i++) { // for loop code block
-			if (i % 1/i == 0) {
-				System.out.println(i);
-			}
+		for (float i = 2; i <= 10; i++) {
+			System.out.printf("%.2f", 1 / i);
+			System.out.println("");
 		}
-
 	}
+
 }
