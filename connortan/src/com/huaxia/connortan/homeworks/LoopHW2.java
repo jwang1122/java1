98ec206d451d999e2cf627838e5b824b16f657e4
connortan1 <cyxt2008@gmail.com>
2021-06-27 22:17:03 -0500

    DOne

--- connortan/src/com/huaxia/connortan/homeworks/LoopHW2.java
+++ connortan/src/com/huaxia/connortan/homeworks/LoopHW2.java
@@ -3,7 +3,7 @@ package com.huaxia.connortan.homeworks;
 public class LoopHW2 {
 
 	public static void main(String[] args) {
-		for (float i = 2; i <= 10; i++) {
+		for (double i = 2; i <= 10; i++) {
 			System.out.printf("%.2f", 1 / i);
 			System.out.println("");
 		}
--- connortan/src/com/huaxia/connortan/homeworks/LoopHW4.java
+++ connortan/src/com/huaxia/connortan/homeworks/LoopHW4.java
@@ -2,4 +2,18 @@ package com.huaxia.connortan.homeworks;
 
 public class LoopHW4 {
 
+	public static void main(String[] args) {
+		for (int a = 0; a <= 5; a++) {
+			for (int c = 0; c < a; c++)
+				System.out.print("* ");
+			System.out.println("");
+
+		}
+		for (int f = 0; f <= 4; f++) {
+			for (int b = 4; b > f; b--)
+				System.out.print("* ");
+			System.out.println("");
+		}
+
+	}
 }
