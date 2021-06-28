a9c99d00a53e73b0198cafce14ee68ac9c8f388b
qiaoemma9 <qiaoemma9@gmail.com>
2021-06-28 10:13:31 -0500

    Finished Variable01

---
+++ emma/src/com/huaxia/emma/homeworks/Variable01.java
@@ -0,0 +1,25 @@
+package com.huaxia.emma.homeworks;
+/** 
+ * My application shows the amount of money left in your bank account after three withdrawals 
+ * @author user
+ *
+ */
+public class Variable01 {
+
+	public static void main(String[] args) {
+		int bankAmount=100;
+		int withdrawOne=25;
+		int withdrawTwo=30;
+		int withdrawThree=15;
+		bankAmount-=bankAmount-withdrawOne;
+		bankAmount-=bankAmount-withdrawTwo;
+		bankAmount-=bankAmount-withdrawThree;
+		System.out.println(bankAmount + " dollars is left");
+		
+		
+	
+		
+				 
+	}
+
+}
