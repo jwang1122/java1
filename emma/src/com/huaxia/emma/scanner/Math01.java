e55c58191795db80922f78ce5757190016e28d1f
qiaoemma9 <qiaoemma9@gmail.com>
2021-06-30 09:00:45 -0500

    Finished hw

---
+++ emma/src/com/huaxia/emma/scanner/Math01.java
@@ -0,0 +1,17 @@
+package com.huaxia.emma.scanner;
+
+import java.util.Scanner;
+
+public class Math01 {
+
+	public static void main(String[] args) {
+		Scanner input= new Scanner(System.in);
+		System.out.println("Enter a radius of a circle(2.0):");
+		double r= input.nextDouble();
+		input.close();
+		double d= 2*r;
+		System.out.printf("The diameter of a circle with radius %.1f is %.1f.", r, d);
+		
+	}
+
+}
