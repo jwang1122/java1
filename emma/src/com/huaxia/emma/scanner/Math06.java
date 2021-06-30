1305b55943890be7a909d9fbf14863a8c7dec8a9
jwang1122 <jwang1122@gmail.com>
2021-06-30 10:38:52 -0500

    help emma little bit on homeworks

--- emma/src/com/huaxia/emma/scanner/Math06.java
+++ emma/src/com/huaxia/emma/scanner/Math06.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
 public class Math06 {
 
 	public static void main(String[] args) {
-		Scanner input= new Scanner(System.in);
-		System.out.print("Enter radius and height of a cylinder (1 1): ");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter radius and height of a cylinder (1.1 1.2): ");
 		double r = input.nextDouble();
-		double h= input.nextDouble();
-		input.close(); {
-		double sa = 2*Math.PI*Math.pow(r,2)+h*2*Math.PI*r;
-		System.out.printf("The cylinders surface area with radius=%.3f and height=%.3f is %.3f.", r, h, sa);
+		double h = input.nextDouble();
+		input.close();
+		{
+			double sa = 2 * Math.PI * Math.pow(r, 2) + h * 2 * Math.PI * r;
+			System.out.printf("The cylinders surface area with radius=%.3f and height=%.3f is %.3f.", r, h, sa);
+		}
 	}
-
 }
-
-}
-
--- emma/src/com/huaxia/emma/scanner/Math11.java
+++ emma/src/com/huaxia/emma/scanner/Math11.java
@@ -5,16 +5,13 @@ import java.util.Scanner;
 public class Math11 {
 
 	public static void main(String[] args) {
-	Scanner input= new Scanner(System.in);
-	System.out.print("Please enter a temperature(45)");
-	double f= input.nextDouble();
-	input.close();
-	double c = 1.8*f + 32;
-	System.out.printf("%.1f degrees Celsius to Farenheit is %.1f",f, c);
-	
-	
-	
-
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter a temperature(45)");
+		double c = input.nextDouble();
+		input.close();
+		
+		// f = 9/5*c + 32
+		double f = 9*c/5 + 32;
+		System.out.printf("%.1f degrees Celsius to Farenheit is %.1f", c, f);
 	}
-
 }
