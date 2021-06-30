7655d943fdeacb77c3430b3add9b2442ce6caa8a
sarahl <sarahl16927@gmail.com>
2021-06-30 10:13:29 -0500

    Finished HW

--- sarah/src/com/huaxia/sarah/homework/LoopHW2.java
+++ sarah/src/com/huaxia/sarah/homework/LoopHW2.java
@@ -5,7 +5,7 @@ public class LoopHW2 {
 	public static void main(String[] args) {
 		for(double i = 2.00; i <= 10.00; i++) {
 			double j = 1.00/i;
-			System.out.println(Math.round(j * 100.00)/100.00);
+			System.out.printf("%.2f\n", j);
 		}
 
 	}
--- sarah/src/com/huaxia/sarah/homework/LoopHW4.java
+++ sarah/src/com/huaxia/sarah/homework/LoopHW4.java
@@ -1,18 +1,33 @@
 package com.huaxia.sarah.homework;
 
+/**
+ *  output:
+ *  *
+ *  * *
+ *  * * *
+ *  * * * *
+ *  * * * * *
+ *  * * * *
+ *  * * *
+ *  * *
+ *  *
+ * @author GLiang
+ *
+ */
+
 public class LoopHW4 {
 
 	public static void main(String[] args) {
 		for (int i = 0; i <= 4; i++) {
 			for (int x = 0; x <= i; x++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 			System.out.println();
 		}	
 			
 		for (int y = 3; y >= 0; y--){
 			for (int x = 0; x <= y; x++) {
-				System.out.print("*");
+				System.out.print("* ");
 			}
 		System.out.println();
 		}
