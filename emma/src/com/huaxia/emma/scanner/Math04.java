cc4e342ca9172a72600ed3c9be778148f1ba7b8c
qiaoemma9 <qiaoemma9@gmail.com>
2021-06-30 09:57:11 -0500

    Finished HW

--- emma/src/com/huaxia/emma/scanner/Math04.java
+++ emma/src/com/huaxia/emma/scanner/Math04.java
@@ -11,7 +11,7 @@ public class Math04 {
 		input.close();
 		{
 			double v = Math.PI * Math.pow(r, 2) * 4;
-			System.out.printf("The spheres surface area with radius=%.3f is %.3f.", r, v);
+			System.out.printf("The spheres surface area with a radius=%.3f is %.3f.", r, v);
 		}
 
 	}
