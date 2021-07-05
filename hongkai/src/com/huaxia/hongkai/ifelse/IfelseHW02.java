ad2792a17ca53b107f9560e46e90364ac8d235c1
hongkaizhang11 <hongkaizhang11@gmail.com>
2021-07-05 12:47:13 -0500

    IfelseHW02 and Loop10 modifications

--- hongkai/src/com/huaxia/hongkai/ifelse/IfelseHW02.java
+++ hongkai/src/com/huaxia/hongkai/ifelse/IfelseHW02.java
@@ -1,22 +1,19 @@
 package com.huaxia.hongkai.ifelse;
 
 /**
- * * Write a Java program to print numbers between 1 to 100 which are divisible by 3, 5 and by both. 
-
-Expected output:
-
-```
-Divided by 3:                                                          
-3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57
-, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99,              
-                                                                       
-Divided by 5:                                                          
-5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 
-95,                                                                    
-                                                                       
-Divided by 3 & 5:                                                      
-15, 30, 45, 60, 75, 90,
-```
+ * * Write a Java program to print numbers between 1 to 100 which are divisible
+ * by 3, 5 and by both.
+ * 
+ * Expected output:
+ * 
+ * ``` Divided by 3: 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45,
+ * 48, 51, 54, 57 , 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99,
+ * 
+ * Divided by 5: 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80,
+ * 85, 90, 95,
+ * 
+ * Divided by 3 & 5: 15, 30, 45, 60, 75, 90, ```
+ * 
  * @author Hongkai Z
  *
  */
@@ -24,20 +21,18 @@ Divided by 3 & 5:
 public class IfelseHW02 {
 
 	public static void main(String[] args) {
-		System.out.println("Divided by 3: ");
-		for (int i = 1; i <= 100; ++i) {
-			if (i % 3 == 0) {
+		dividedBy("3", 3);
+		dividedBy("5", 5);
+		dividedBy("3 & 5", 15);
+	}
+
+	static void dividedBy(String say, int num) {
+		System.out.printf("Divided by %s: \n", say);
+		for (int i = 1; i < 100; ++i) {
+			if (i % num == 0) {
 				System.out.print(i + ", ");
-			}} System.out.println();System.out.println();
-		System.out.println("Divided by 5: ");
-		for (int i = 1; i <= 100; ++i) {
-			if (i % 5 == 0) {
-				System.out.print(i + ", ");
-			}} System.out.println();System.out.println();
-		System.out.println("Divided by 3 & 5: ");
-		for (int i = 1; i <= 100; ++i) {
-			if (i % 15 == 0) {
-				System.out.print(i + ", ");
-			}} System.out.println();System.out.println();
+			}
+		}
+		System.out.println("\n");
 	}
 }
