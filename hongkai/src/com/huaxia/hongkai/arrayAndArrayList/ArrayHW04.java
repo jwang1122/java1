ab9c6e9d7a0ed6119d31012cc8d25ec93ebc984b
hongkaizhang11 <hongkaizhang11@gmail.com>
2021-07-08 19:08:50 -0500

    ArrayHW04-06

--- hongkai/src/com/huaxia/hongkai/arrayAndArrayList/ArrayHW04.java
+++ hongkai/src/com/huaxia/hongkai/arrayAndArrayList/ArrayHW04.java
@@ -19,9 +19,20 @@ Average value of the array elements is : 7.0
 
 public class ArrayHW04 {
 
+	// method for calculating average of items in int[] array list
+	public static int arrayItemAverage(int[] arrayInt) {
+		int methodReturn = 0;
+		for (int arrayIntIndex:arrayInt) {
+			methodReturn += arrayIntIndex;
+		}
+		methodReturn /= arrayInt.length;
+		return methodReturn;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] numbers = new int[]{20, 30, 25, 35, -16, 60, -100};
+		int average = arrayItemAverage(numbers);
+		System.out.printf("Average value of the array elements is : %d", average);
 	}
 
 }
--- hongkai/src/com/huaxia/hongkai/arrayAndArrayList/ArrayHW05.java
+++ hongkai/src/com/huaxia/hongkai/arrayAndArrayList/ArrayHW05.java
@@ -1,9 +1,76 @@
 package com.huaxia.hongkai.arrayAndArrayList;
 
+/**
+ * * Write a Java program to test if an array contains a specific value.
+
+for example:
+
+```java
+          int[] my_array1 = {
+            1789, 2035, 1899, 1456, 2013, 
+            1458, 2458, 1254, 1472, 2365, 
+            1456, 2265, 1457, 2456};
+
+        System.out.println(contains(my_array1, 2013));
+        System.out.println(contains(my_array1, 2015));
+
+```
+
+Expected output:
+
+true
+false
+
+or:
+
+The array contains 2013.
+The array does not contain 2015.
+
+
+ * @author Hongkai Z
+ *
+ */
+
 public class ArrayHW05 {
 
+/**
+	//Method #1
+	private static boolean contains(int[] my_array1, int matchItem) {
+		boolean containsItem = false;
+		for (int arrayItem : my_array1) {
+			if (arrayItem == matchItem) {
+				containsItem = true;
+				break;
+			}
+		}
+		return containsItem;
+	}
+ */	
+
+	//Method #2
+	private static String contains(int[] my_array1, int matchItem) {
+		boolean containsItem = false;
+		for (int arrayItem : my_array1) {
+			if (arrayItem == matchItem) {
+				containsItem = true;
+				break;
+			}
+		}
+		String str1 = "The array contains %d.";
+		if (containsItem == false) {
+			str1 = "The array does not contain %d.";
+		}
+		String str2 = String.format(str1, matchItem);
+		return str2;
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] my_array1 = {
+			1789, 2035, 1899, 1456, 2013, 
+			1458, 2458, 1254, 1472, 2365, 
+			1456, 2265, 1457, 2456};
+        System.out.println(contains(my_array1, 2013));
+        System.out.println(contains(my_array1, 2015));
 
 	}
 
