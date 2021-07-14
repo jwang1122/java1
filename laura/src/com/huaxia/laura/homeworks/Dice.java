23fc629ba8b9bc788f71453ed8502f8e9bd5c7bb
lauraliang06 <lauraliang06@icloud.com>
2021-07-13 23:10:16 -0500

    hw

--- laura/src/com/huaxia/laura/homeworks/Dice.java
+++ laura/src/com/huaxia/laura/homeworks/Dice.java
@@ -12,13 +12,13 @@ public class Dice
 		boolean playing = true;
 		do 
 		{
-			System.out.print("John's turn: ");
+			System.out.print("Player 1's turn: ");
 			input.nextLine();
 			int dice1 = rand.nextInt(6) + 1;
 			int dice2 = rand.nextInt(6) + 1;
 			System.out.println(dice1 + " + " + dice2 + " = " + (dice1 + dice2));
 		
-			System.out.print("Connor's turn: ");
+			System.out.print("Player 2's turn: ");
 			input.nextLine();
 			int dice3 = rand.nextInt(6) + 1;
 			int dice4 = rand.nextInt(6) + 1;
@@ -28,11 +28,11 @@ public class Dice
 			int P2 = dice3 + dice4;
 			if(P1 > P2) 
 			{
-				System.out.println("John wins!");
+				System.out.println("Player 1 wins!");
 			}
 			else if(P1 < P2) 
 			{
-				System.out.println("Connor wins!");			
+				System.out.println("Player 2 wins!");			
 			}
 			else 
 			{
